6fb7056b007c9f9fc1fcefc755502122333fc38a
xzhang35-kc <69605329+xzhang35-kc@users.noreply.github.com>
2021-03-11 01:11:30 -0500

    Add files via upload

--- Ordinal/CProg/lib-PX-MH/MH.cpp
+++ Ordinal/CProg/lib-PX-MH/MH.cpp
@@ -9,71 +9,12 @@
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_cdf.h>
 #include <gsl/gsl_linalg.h>
+
 #include "Matrix.h"
+#include "Random.h"
 
 extern gsl_rng * r;
 
-
-// generate a random matrix from Wishart Distribution with df = m and sigma.
-// all the matrix have been allocated and sigma will not be changed
-// in this routine;
-void MHWishart(gsl_matrix * w, const int & m, gsl_matrix * sigma)
-{
-	int K = sigma -> size1;
-	symmetric(sigma);
-	gsl_matrix * Sig = gsl_matrix_calloc (K, K);
-	gsl_matrix * L = gsl_matrix_calloc (K, K);
-	gsl_matrix * T1 = gsl_matrix_alloc (K, K);
-	gsl_matrix * T2 = gsl_matrix_alloc (K, K);
-	gsl_matrix * Q = gsl_matrix_alloc (K, K);
-
-	// copy sigma to another matrix, so sigma does not change in this routine;
-	gsl_matrix_memcpy(Q,sigma);
-
-	// choleskey decomposition
-	gsl_linalg_cholesky_decomp(Q);
-
-	//get the lower triangle matrix Sig
-	double a;
-	for(int i = 0; i < K; i++)
-	{
-	  for(int j = 0; j <= i; j++)
-	  {
-		  a = gsl_matrix_get(Q, i, j);
-		  gsl_matrix_set (Sig, i, j, a);
-	  }
-	}
-
-	// generate random low triangle matrix L
-	// based on Barlett's decomposition for lower triangular matrix.
-    for (int i = 0; i < K; i++)
-	{
-		for (int j = 0; j < K; j++)
-		{
-			if(i == j)
-			{
-				a = sqrt(gsl_ran_chisq (r, m-i));
-				gsl_matrix_set (L, i, j, a);
-			}
-			else if(i > j)
-			{
-				a = gsl_ran_gaussian(r, 1.0);
-				gsl_matrix_set (L, i, j, a);
-			}
-		}
-	}
-
-	gsl_blas_dgemm (CblasNoTrans, CblasNoTrans, 1.0, Sig, L, 0.0, T1);
-    gsl_blas_dgemm (CblasNoTrans, CblasTrans, 1.0, T1, L, 0.0, T2);
-	gsl_blas_dgemm (CblasNoTrans, CblasTrans, 1.0, T2, Sig, 0.0, w);
-
-	// free of memory;
-	gsl_matrix_free(Sig);
-	gsl_matrix_free(L);
-	gsl_matrix_free(T1);
-	gsl_matrix_free(T2);
-	gsl_matrix_free(Q);
-}
 
 // obtain covariance matrix RR from WR for mixed data which used in the posterior density calculation
 void Transform(gsl_matrix * RR, gsl_matrix * WR, const int & bc)
@@ -105,51 +46,6 @@ void Transform(gsl_matrix * RR, gsl_matrix * WR, const int & bc)
 	gsl_matrix_free(DI);
 	gsl_matrix_free(DI1);
 	gsl_matrix_free(R);
-}
-
-// obtain a random number from multivariate normal with mean mu and covariance matrix sigma.
-// mu and sigma will not be changed in this routine;
-// In this routine, the random vector is treated as a matrix.
-void MHmultinorm(gsl_matrix * Mnorm, gsl_matrix * mu, gsl_matrix * sigma)
-{
-	int K = sigma -> size1;
-
-	gsl_matrix * Sig = gsl_matrix_calloc(K, K);
-	gsl_matrix * Q = gsl_matrix_alloc(K, K);
-	gsl_matrix * M = gsl_matrix_alloc(K, 1);
-
-	// first generate k independent random variables from standard normal
-	double a;
-	for(int i = 0; i < K; i++)
-	{
-		a = gsl_ran_gaussian(r, 1.0);
-		gsl_matrix_set(M, i, 0, a);
-	}
-
-	// copy sigma to Q;
-	symmetric(sigma);
-	gsl_matrix_memcpy(Q,sigma);
-
-	// choleskey decomposition
-	gsl_linalg_cholesky_decomp(Q);
-
-	//get the lower triangle matrix Sig
-	for(int i = 0; i < K; i++)
-	{
-	    for(int j = 0; j <= i; j++)
-        {
-            a = gsl_matrix_get (Q, i, j);
-            gsl_matrix_set (Sig, i, j, a);
-        }
-    }
-
-	gsl_blas_dgemm (CblasNoTrans, CblasNoTrans, 1.0, Sig, M, 0.0, Mnorm);
-	gsl_matrix_add (Mnorm, mu);
-
-	// free memory
-	gsl_matrix_free(Sig);
-	gsl_matrix_free(Q);
-	gsl_matrix_free(M);
 }
 
 // compute the prior for both R and D,
@@ -223,7 +119,8 @@ double Posterior(const int & N, gsl_matrix * R, gsl_matrix * Z, gsl_matrix * XB)
 	}
 
 	Inv(Siginv, Sig);
-	double a, Txx;
+	double a;
+	double Txx = 0;
 	for(int i = 0; i < N; i++)
 	{
 		for(int j = 0; j < K; j++)
@@ -272,7 +169,7 @@ int MHsampler(gsl_matrix * WR, gsl_matrix * Z, gsl_matrix * XB, gsl_matrix * Psi
 	gsl_matrix_memcpy(Wrr, WR);
 	gsl_matrix_scale(Wrr, 1.0 / m);
 
-	MHWishart(Wc, m, Wrr);
+	Wishart(Wc, m, Wrr);
 
 	gsl_matrix_memcpy(Wcc, Wc);
 	gsl_matrix_scale(Wcc, 1.0 / m);
