a4ab197416ff7d3551c3f74065ad3de4c026d0ce
xzhang35-kc <69605329+xzhang35-kc@users.noreply.github.com>
2022-05-13 13:36:28 -0400

    Add files via upload

---
+++ Ordinal/CProg/lib-gs/GS.cpp
@@ -0,0 +1,5 @@
+/////////////////////////////////////
+// Name: lib-gs/GS.cpp
+// Developers: Xiao & Kui Zhang at MTU
+// Copyright: 2005 -
+// Description: functions related to GS method
---
+++ Ordinal/CProg/lib-gs/GS.h
@@ -0,0 +1,6 @@
+/////////////////////////////////////
+// Name: lib-gs/GS.h
+// Developers: Xiao & Kui Zhang at MTU
+// Copyright: 2005 -
+// Description: functions related to GS method, header file
+
